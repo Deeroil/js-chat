ea7b94db584fe2fc6923143fd4f1aa2f934e052f
Miina Saroma <miinasaroma@gmail.com>
2021-02-27 19:40:33 +0200

    Update table name, generate UUIDs

--- sql/init.sql
+++ sql/init.sql
@@ -1,26 +1,26 @@
-CREATE TABLE "User" (
-    id UUID PRIMARY KEY,
-    name TEXT NOT NULL,
-    created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now()
+CREATE TABLE Account (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT UNIQUE NOT NULL,
+  created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now()
 );
 
 CREATE TABLE Channel (
-    id UUID PRIMARY KEY,
-    name TEXT
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT
 );
 
 CREATE TABLE Message (
-    id UUID PRIMARY KEY,
-    sender UUID NOT NULL, -- foreign key?
-    channel UUID NOT NULL, -- foreign key?
-    text TEXT,
-    media BYTEA,
-    media_type TEXT,
-    created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now()
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  sender UUID NOT NULL, -- foreign key?
+  channel UUID NOT NULL, -- foreign key?
+  text TEXT,
+  media BYTEA,
+  media_type TEXT,
+  created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now()
 );
 
-CREATE TABLE Channel_User (
-    channel UUID NOT NULL, -- foreign key?
-    "user" UUID NOT NULL, -- foreign key?
-    last_seen UUID
+CREATE TABLE Channel_Account (
+  channel UUID NOT NULL, -- foreign key?
+  account UUID NOT NULL, -- foreign key?
+  last_seen UUID -- foreign key?
 );
