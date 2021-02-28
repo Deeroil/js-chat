c004f630640ff69a4f2be3e3c96f63bbd5e4e3a8
Miina Saroma <miinasaroma@gmail.com>
2021-02-28 13:15:48 +0200

    Move app to its own file

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const accountRouter = require('./controllers/account')
+const { pool } = require('./sql')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cookieParser(process.env.SECRET))
+app.use(express.static('frontend'))
+
+app.use('/api/account', accountRouter)
+
+app.get('/postgres', async (req, res) => {
+  const { rows } = await pool.query('SELECT version()')
+  res.json(rows[0])
+})
+
+module.exports = app
--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,9 @@
 require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const app = require('./app')
 const http = require('http')
-const { pool } = require('./sql')
-const accountRouter = require('./controllers/account')
 
-const app = express()
 const server = http.createServer(app)
 const PORT = process.env.PORT || 3003
-
-app.use(bodyParser.json())
-app.use(cookieParser(process.env.SECRET))
-app.use(express.static('frontend'))
-
-app.use('/api/account', accountRouter)
-
-app.get('/postgres', async (req, res) => {
-  const { rows } = await pool.query('SELECT version()')
-  res.json(rows[0])
-})
 
 server.listen(PORT, () => {
   console.log(`Running in http://localhost:${PORT}`)
