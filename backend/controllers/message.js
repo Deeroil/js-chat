383fdaa87f162c24754cb9c807279fd624639ca8
Miina Saroma <miinasaroma@gmail.com>
2021-04-18 13:01:52 +0300

    Get messages on a channel

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -25,4 +25,23 @@ messageRouter.get('/all', async (req, res) => {
   res.json(rows)
 })
 
+//TODO: check if logged in account is on the channel = has permission
+// if channel doesn't exist
+//should I rather send channel in body
+//move to /api/channel? maybe /api/channel/messages
+messageRouter.get('/:channel', async (req, res) => {
+  const channelUUID = req.params.channel
+
+  // should throw error if channel doesn't exist
+
+  const { rows } = await pool.query(
+    `SELECT text, name, Message.created
+    FROM Message JOIN Account ON Account.id = Message.sender
+    WHERE Message.channel = ($1)`,
+    [channelUUID]
+  )
+
+  res.json(rows)
+})
+
 module.exports = messageRouter
